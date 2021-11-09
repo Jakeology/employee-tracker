3cae7c0df32b616f5769ac40ff391fc152331ab2
Jake <xxjakexx14@gmail.com>
2021-11-09 11:01:57 -0500

    added menu inquirer prompts

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const inquirer = require("inquirer");
 
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: [
+          "view all departments",
+          "view all roles",
+          "view all employees",
+          "add a department",
+          "add a role",
+          "add an employee",
+          "update an employee role",
+        ],
+      },
+    ])
+    .then((choice) => {
+      switch (choice.menu) {
+        
+      }
+    });
+}
+
+menu();
