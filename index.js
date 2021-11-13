4744be64169a24069456ed800dd9a71b4431fa78
Jake <xxjakexx14@gmail.com>
2021-11-13 18:47:13 -0500

    added functions to return view all department and role data

--- index.js
+++ index.js
@@ -34,11 +34,35 @@ function menu() {
         case "View All Employees":
           viewAllEmployees();
           break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
       }
     });
 
   function viewAllEmployees() {
     const sql = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id`;
+
+    db.query(sql, function (err, results) {
+      if (err) throw err;
+
+      console.log("");
+      console.table(results);
+      menu();
+    });
+  }
+
+  function viewAllDepartments() {
+    console.log("");
+    console.table(departmentData);
+    menu();
+  }
+
+  function viewAllRoles() {
+    const sql = `SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id`;
 
     db.query(sql, function (err, results) {
       if (err) throw err;
